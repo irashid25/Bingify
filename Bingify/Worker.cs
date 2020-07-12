9c5b68d6bc71175665730c84df5e9f96db82da40
irashid25 <irashid25@gmail.com>
2020-07-12 01:35:44 +0100

    small refactor.

--- Bingify/Worker.cs
+++ Bingify/Worker.cs
@@ -23,103 +23,95 @@ namespace Bingify
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            // check if directories exist - should have read permissions on these folders
-            if (!Directory.Exists(SPOTLIGHT_FOLDER_PATH))
+            try
             {
-                this.logger.LogError("Windows spotlight is not enabled");
-                throw new Exception();
-            }
-
-            if (!Directory.Exists(PICTURE_FOLDER))
-            {
-                this.logger.LogError("Picture folder does not exist");
-                throw new Exception();
-            }
-
-            if (!CheckIfUserHasReadWriteAccess(SPOTLIGHT_FOLDER_PATH, PICTURE_FOLDER))
-            {
-                this.logger.LogError($"Please check the user has access to the following folders: {SPOTLIGHT_FOLDER_PATH}, {PICTURE_FOLDER}");
-                throw new Exception();
-            }
-
-            if (!Directory.Exists(WALLPAPER_FOLDER))
-            {
-                Directory.CreateDirectory(WALLPAPER_FOLDER);
-            }
-            
-
-            if (this.watcher == null)
-            {
-                this.watcher = new FileSystemWatcher(SPOTLIGHT_FOLDER_PATH);
-                this.watcher.Changed += this.WatcherFolderChanged;
-                this.watcher.EnableRaisingEvents = true;
                 this.logger.LogInformation("Bingify running at: {time}", DateTimeOffset.Now);
+
+                // check if directories exist - should have read permissions on these folders
+                if (!Directory.Exists(SPOTLIGHT_FOLDER_PATH))
+                {
+                    this.logger.LogError("Windows spotlight is not enabled");
+                    throw new Exception();
+                }
+
+                if (!Directory.Exists(PICTURE_FOLDER))
+                {
+                    this.logger.LogError("Picture folder does not exist");
+                    throw new Exception();
+                }
+
+                if (!CheckIfUserHasReadWriteAccess(SPOTLIGHT_FOLDER_PATH, PICTURE_FOLDER))
+                {
+                    this.logger.LogError($"Please check the user has access to the following folders: {SPOTLIGHT_FOLDER_PATH}, {PICTURE_FOLDER}");
+                    throw new Exception();
+                }
+
+                if (!Directory.Exists(WALLPAPER_FOLDER))
+                {
+                    Directory.CreateDirectory(WALLPAPER_FOLDER);
+                }
+
+                if (this.watcher == null)
+                {
+                    this.watcher = new FileSystemWatcher(SPOTLIGHT_FOLDER_PATH);
+                    this.watcher.Changed += this.WatcherFolderChanged;
+                    this.watcher.EnableRaisingEvents = true;
+                }
+
+                await Task.CompletedTask;
             }
-            else
+            catch (Exception ex)
             {
-                this.watcher.Changed -= this.WatcherFolderChanged;
-                this.watcher = null;
-            }
-
-
-            await Task.CompletedTask;
-            
+                this.logger.LogInformation($"There was an error running the bingify service: {ex}");
+            } 
         }
 
         private void WatcherFolderChanged(object sender, FileSystemEventArgs e)
         {
-            try
+            if (e.ChangeType == WatcherChangeTypes.Changed && e.ChangeType != WatcherChangeTypes.Deleted)
             {
-                if (e.ChangeType == WatcherChangeTypes.Changed && e.ChangeType != WatcherChangeTypes.Deleted)
+                // new file been added/modified - we going to check if the jpg already exists before saving so this should not be an issue
+                var fileInfo = new FileInfo(e.FullPath);
+                if (!fileInfo.Exists)
                 {
-                    // new file been added/modified - we going to check if the jpg already exists before saving so this should not be an issue
-                    var fileInfo = new FileInfo(e.FullPath);
-                    if (!fileInfo.Exists)
-                    {
-                        this.logger.LogError($"Cant get created/modified file in the spotlight folder: {SPOTLIGHT_FOLDER_PATH}");
-                        return;
-                    }
+                    this.logger.LogError($"Cant get created/modified file in the spotlight folder: {SPOTLIGHT_FOLDER_PATH}");
+                    return;
+                }
 
-                    var extention = Path.GetExtension(e.FullPath);
-                    if (string.Compare(extention, JPEG_EXTENSION, StringComparison.InvariantCultureIgnoreCase) == 0)
-                    {
-                        this.logger.LogError($"{e.FullPath} is already a image file");
-                        return;
-                    }
+                var extention = Path.GetExtension(e.FullPath);
+                if (string.Compare(extention, JPEG_EXTENSION, StringComparison.InvariantCultureIgnoreCase) == 0)
+                {
+                    this.logger.LogError($"{e.FullPath} is already a image file");
+                    return;
+                }
 
-                    // check file is potentially an image (going to use min size of 400kb for now)
-                    if (fileInfo.Length < 400000)
-                    {
-                        this.logger.LogError($"{e.FullPath} is too small for an image");
-                        return;
-                    }
+                // check file is potentially an image (going to use min size of 400kb for now)
+                if (fileInfo.Length < 400000)
+                {
+                    this.logger.LogError($"{e.FullPath} is too small for an image");
+                    return;
+                }
 
 
-                    // check if the pictures folder contains the new file
-                    var newImageFile = Path.ChangeExtension(e.FullPath, JPEG_EXTENSION);
-                    var newImageFileName = Path.GetFileName(newImageFile);
+                // check if the pictures folder contains the new file
+                var newImageFile = Path.ChangeExtension(e.FullPath, JPEG_EXTENSION);
+                var newImageFileName = Path.GetFileName(newImageFile);
 
-                    var existingWallpapers = Directory.GetFiles(WALLPAPER_FOLDER);
+                var existingWallpapers = Directory.GetFiles(WALLPAPER_FOLDER);
 
-                    if (existingWallpapers == null || !existingWallpapers.Any())
+                if (existingWallpapers == null || !existingWallpapers.Any())
+                {
+                    File.Copy(e.FullPath, Path.Combine(WALLPAPER_FOLDER, newImageFileName));
+                }
+                else
+                {
+                    if (!File.Exists(Path.Combine(WALLPAPER_FOLDER, newImageFileName)))
                     {
                         File.Copy(e.FullPath, Path.Combine(WALLPAPER_FOLDER, newImageFileName));
                     }
-                    else
-                    {
-                        if (!File.Exists(Path.Combine(WALLPAPER_FOLDER, newImageFileName)))
-                        {
-                            File.Copy(e.FullPath, Path.Combine(WALLPAPER_FOLDER, newImageFileName));
-                        }
-                    }
                 }
             }
-            catch (Exception ex)
-            {
-                this.logger.LogError($"There was an error creating the file: {ex}");
-                return;
-            }
-            
+
         }
 
         private bool CheckIfUserHasReadWriteAccess(params string[] paths)
@@ -127,20 +119,12 @@ namespace Bingify
             var hasAccess = true;
             foreach(var path in paths)
             {
-                try
-                {
-                    var newFile = @$"{path}\test.txt";
-                    File.WriteAllBytes(newFile, new byte[0]);
+                var newFile = @$"{path}\test.txt";
+                File.WriteAllBytes(newFile, new byte[0]);
 
-                    // user has write access, delete the test one
-                    File.Delete(newFile);
-                    continue;
-                }
-                catch (Exception ex)
-                {
-                    hasAccess = false;
-                    this.logger.LogError($"User doesnt have write access to directory: {path}");
-                }
+                // user has write access, delete the test one
+                File.Delete(newFile);
+                continue;
             }
 
             return hasAccess;
