2ceb40c1cde93703670448aa4674f234c1fd9cbb
irashid25 <irashid25@gmail.com>
2020-07-12 20:40:30 +0100

    actually updated the powershell script now. :)

--- Bingify/install.ps1
+++ Bingify/install.ps1
@@ -1,12 +1,28 @@
-param ($serviceExeFolderLocation, $serviceExeLocation, $domainUserName, $serviceName)
-write-host $serviceExeLocation
-write-host $domainUserName
-write-host $serviceName
+param ($serviceExeFolderLocation, $serviceName)
+
+$MyCredential = Get-Credential
+
+$domainUserName = $MyCredential.UserName
+
+$password = $MyCredential.GetNetworkCredential().password
+
+Write-Host $domainUserName
+Write-Host $password
+
+
 
 $acl = Get-Acl $serviceExeFolderLocation
 $aclRuleArgs = $domainUserName, "Read,Write,ReadAndExecute", "None", "None", "Allow"
 $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($aclRuleArgs)
 $acl.SetAccessRule($accessRule)
-$acl | Set-Acl "$serviceExeLocation"
+$acl | Set-Acl "$serviceExeFolderLocation\Bingify.exe"
 
-New-Service -Name $serviceName -BinaryPathName $serviceExeLocation -Credential $domainUserName -Description "Bingify Service" -DisplayName $serviceName -StartupType Automatic
+$securePassword = convertto-securestring -String $password -AsPlainText -Force  
+$credentialsObject = new-object -typename System.Management.Automation.PSCredential -argumentlist $domainUserName, $securePassword
+
+
+New-Service -Name $serviceName -BinaryPathName "$serviceExeFolderLocation\Bingify.exe" -Credential $credentialsObject -Description "Bingify Service" -DisplayName $serviceName -StartupType Automatic
+
+Start-Service -Name $serviceName
+
+Get-Service -Name $serviceName
